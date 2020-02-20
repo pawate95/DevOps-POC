afd767a628463e4affbff1028b71ab219d1cfbe1
pawate95 <59087855+pawate95@users.noreply.github.com>
2020-02-20 10:12:36 +0000

    Update acat.java

--- Acceptancetest/src/test/java/acceptancetest/acat.java
+++ Acceptancetest/src/test/java/acceptancetest/acat.java
@@ -12,10 +12,10 @@ public class acat {
 	public HtmlUnitDriver driver;
 	//public WebDriver driver;
 	public WebDriverWait wait;
-	public String URL = "http://18.222.108.222:8080/ProdWebapp";
-	public String URL1 = "http://18.222.108.222:8080/ProdWebapp/avncreatepage.jsp";
-	//public String URL = "http://localhost:8080/AVNCommunication-1.0/avnlogin.jsp";
-	//public String URL1 = "http://localhost:8080/AVNCommunication-1.0/avncreatepage.jsp";
+	//public String URL = "http://18.222.108.222:8080/ProdWebapp";
+	//public String URL1 = "http://18.222.108.222:8080/ProdWebapp/avncreatepage.jsp";
+	public String URL = "http://localhost:8080/JavaWebApp-1.0/avnlogin.jsp";
+	public String URL1 = "http://localhost:8080/JavaWebApp-1.0/avncreatepage.jsp";
 	
 	@Test(priority = 0)
 	public void checkin() {
